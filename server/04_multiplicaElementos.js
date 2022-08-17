45c0c0f3797f24f4abee8f5e4462ff50d5027676
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-17 13:23:11 -0500

    Playground - 1

---
+++ server/04_multiplicaElementos.js
@@ -0,0 +1,10 @@
+//Multiplica todos los elementos por 2
+function solution(array){
+    const respuesta = array.map(element => {
+            return element*2;
+        })
+    console.log(respuesta);
+}
+const arr = [2,4,5,6]
+solution(arr);
+
