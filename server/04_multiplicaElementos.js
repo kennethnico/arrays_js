38a9beb8c677f7896fc3bd779d2ca521dd85097e
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-17 13:42:00 -0500

    Playground - 2

--- server/04_multiplicaElementos.js
+++ server/04_multiplicaElementos.js
@@ -8,3 +8,25 @@ function solution(array){
 const arr = [2,4,5,6]
 solution(arr);
 
+//Transforma un array de objetos a un array de Strings
+const obj = [
+    {
+        name: 'Edwin',
+        lastName: 'Nicolas',
+        age: 28,
+    },
+    {
+        name: 'Kenet',
+        lastName: 'Ruiz',
+        age: 28,
+    }
+    ];
+
+function solution2(obj){
+    let r = [];
+    obj.forEach(arr => {
+        r.push(arr.name);
+    });
+    console.log(r);
+}
+solution2(obj);
