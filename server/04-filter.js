c368deff98eed021c6b213d94d5e6c1f37c000d6
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-18 12:44:35 -0500

    Clase 8 - Filter

--- server/04-filter.js
+++ server/04-filter.js
@@ -14,3 +14,24 @@ console.log('Palabras con For',newArray);
 const newArray2 = words.filter(item => item.length>=6);
 console.log('Original con Filter',words);
 console.log('Palabras con Filter',newArray2);
+
+const orders =[
+    {
+        customerName: 'Edwin',
+        total: 60,
+        delivered: true,
+    },
+    {
+        customerName: 'Kenet',
+        total: 100,
+        delivered: true,
+    },
+    {
+        customerName: 'Nicolás',
+        total: 160,
+        delivered: false,
+    },
+];
+const deliveredDone = orders.filter(item => item.delivered && item.total >= 100);
+console.log('Ordenes - ', orders);
+console.log('Orders filters', deliveredDone);
