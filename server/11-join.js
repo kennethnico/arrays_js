834c1874ef701421b4406b58d740ae3393c91f29
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-22 14:53:52 -0500

    11 join - Creando una url

--- server/11-join.js
+++ server/11-join.js
@@ -1,11 +1,20 @@
 //Es un método que nos aydua a unificar un Array
-const elements = ['Fire','Air','Water'];
+const elements = ['Fire','Air','Water','my','item','test'];
 const separator ='--';
 let stringFinal ='';
 for (let i = 0; i < elements.length; i++) {
     let item = elements[i];
-    stringFinal = stringFinal+item+separator;
+    if(i<elements.length-1) {
+        stringFinal = stringFinal+item+separator;
+    }else{
+        stringFinal = stringFinal+item;
+    }
 }
 console.log('Con FOR: ', stringFinal);
 const sFinal2 = elements.join('--');
 console.log('Con JOIN: ', sFinal2);
+
+const title = 'Curso de manipuacion de arrays';
+const mySite = 'https://www.mypersonalsiteweb.com/';
+const myURL = mySite+title.split(' ').join('-').toLowerCase()+'.html';
+console.log(myURL);
