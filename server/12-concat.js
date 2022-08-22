21f1da75ddd8df464235b6674e0366e3b62b1202
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-22 15:02:57 -0500

    12 concat - Funsionando arrays

---
+++ server/12-concat.js
@@ -0,0 +1,12 @@
+//Método para concatenar/fusionar arrays
+const elements =[1,1,2,2];
+const elements2 =[3,3,4,4];
+
+const newArray=elements;
+for (let i = 0; i < elements2.length; i++) {
+    const element = elements2[i];
+    newArray.push(element);
+}
+const newArray2 = elements.concat(elements2);
+console.log(newArray);
+console.log(newArray2);
