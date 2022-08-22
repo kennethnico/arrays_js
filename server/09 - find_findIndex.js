3eb860cb65ea51799cae872471934b6a0aff4d9c
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-22 12:56:55 -0500

    09 fin_finIndex - Productos por ID

--- server/09 - find_findIndex.js
+++ server/09 - find_findIndex.js
@@ -11,3 +11,30 @@ for (let i = 0; i <elements.length; i++) {
 console.log("Con FOR: ",rta);
 const rta2 = elements.find(item => item === 30);
 console.log("Con FIND: ",rta2);
+//Encontrando productos por ID
+const products = [
+    {
+        name: "Pizza",
+        price: 12,
+        id: '🍕'
+    },
+    {
+        name: "Burger",
+        price: 23,
+        id: '🍔'
+    },
+    {
+        name: "Hot dog",
+        price: 34,
+        id: '🌭'
+    },
+    {
+        name: "Hot cakes",
+        price: 355,
+        id: '🥞'
+    },
+];
+const rta3 = products.find(i => i.id=== '🥞');
+console.log("Producto encontrado: ", rta3);
+const rta4 = products.findIndex(i => i.id=== '🥞');
+console.log("Producto encontrado y su posición es: ", rta4);
