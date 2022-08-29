5c626e0ff4c523c9c6932d23fed580f6e07b5c50
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-29 13:18:37 -0500

    14 Flatmap - Método Flatmap

---
+++ server/14-flatmap.js
@@ -0,0 +1,41 @@
+//Método flatmap
+const users = [
+    { userId: 1, username: "Tom", attributes: ["Nice", "Cute"]},
+    { userId: 2, username: "Mike", attributes: ["Lovely"]},
+    { userId: 3, username: "Nico", attributes: ["Nice", "Cool"]},
+];
+const att = users.map(user => user.attributes).flat();
+console.log("Atributos map y flat: ->",att);
+const att2 = users.flatMap(user => user.attributes);
+console.log("Atributos flatMap: ->",att2);
+
+//Calendario - Fechas de inicio de evento
+const calendars = {
+    primaryCalendar: [
+        {
+            startDate: new Date(2021, 1, 1, 15),
+            endDate: new Date(2021, 1, 1, 15, 30),
+            title: "Cita 1",
+        },
+        {
+            startDate: new Date(2021, 1, 1, 17),
+            endDate: new Date(2021, 1, 1, 18),
+            title: "Cita 2",
+        },
+    ],
+    secondaryCalendar: [
+        {
+            startDate: new Date(2021, 1, 1, 12),
+            endDate: new Date(2021, 1, 1, 12, 30),
+            title: "Cita 2",
+        },
+        {
+            startDate: new Date(2021, 1, 1, 9),
+            endDate: new Date(2021, 1, 1, 10),
+            title: "Cita 4",
+        },
+    ],
+};
+
+const sDate = Object.values(calendars).flat().flatMap(item=>item.startDate);
+console.log("Dates, ", sDate);
